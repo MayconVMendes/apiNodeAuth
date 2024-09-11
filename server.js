aa645ca3f45cb0acb01f3fa514a84d88fe9df3e0
Maycon <maycon-big@hotmail.com>
2024-09-11 14:11:57 -0300

    feat: configure to cors

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import express from "express";
 import publicRoutes from "./routes/public.js";
 import privateRoutes from "./routes/private.js";
 import auth from "./middlewares/auth.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+//Configurando o cors, dizendo que qualquer um pode usar a API
+app.use(cors());
 
 app.use("/", publicRoutes);
 
